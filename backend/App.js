0f38d5187c0a175c7f45514465caea2bd7020490
andisulfitriyanti1 <andisulfitriyantisyamsul@gmail.com>
2026-06-11 10:46:19 +0700

    memperbaiki bagian menu certificate sesuai dengan yg tampilan figma/ JUMI

--- backend/App.js
+++ backend/App.js
@@ -1,41 +1,42 @@
+import dotenv from "dotenv";
+// Mengonfigurasi dotenv di baris paling atas agar semua file di bawahnya bisa membaca file .env dengan aman
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import sequelize from "./config/database.js";
 import User from "./models/User.js";
 import "./models/index.js";
-import dotenv from "dotenv";
 import path from "path";
 
-//ROUTES
+// ROUTES
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
 import certificatesRoutes from "./routes/certificatesRoutes.js";
 import skillsRoutes from "./routes/skillsRoutes.js";
 
-dotenv.config();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
+// ROUTES CONFIGURATION
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/profile", profileRoutes);
 app.use("/certificates", certificatesRoutes);
 app.use("/skills", skillsRoutes);
 
-//UPLOAD FILE
+// UPLOAD FILE
 app.use("/uploads", express.static("uploads"));
 
-//TEST API
+// TEST API
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-
+// DATABASE AUTHENTICATION
 sequelize.authenticate()
   .then(() => console.log("Database connected..."))
   .catch(err => console.log("Error: " + err));
@@ -45,7 +46,10 @@ sequelize.sync()
   .then(() => console.log("Table created"))
   .catch(err => console.log(err));
 
-//SERVER
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+/* SERVER CONFIGURATION
+  Menggunakan fallback || 5000 sebagai cadangan jika file .env belum terbaca sempurna 
+*/
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
